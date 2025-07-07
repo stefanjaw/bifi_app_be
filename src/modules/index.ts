d071d7d74e0c6dc896822c4b07b2951ca3c7d393
Elias123Gonzalez <tec4@avalantec.com>
2025-07-07 12:52:18 -0600

    fix: correct import path for productTypeRouter to use hyphenated directory name

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,4 @@
 export { countryRouter } from "./countries/routes/country-routes";
 export { companyRouter } from "./companies/routes/company-routes";
 export { contactRouter } from "./contacts/routes/contact-routes";
-export { productTypeRouter } from "./product_types/routes/product-type-routes";
+export { productTypeRouter } from "./product-types/routes/product-type-routes";
