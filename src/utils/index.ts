df578fc52230f6a87fb73e861752e61b42978a4d
Elias123Gonzalez <tec4@avalantec.com>
2025-07-10 12:53:11 -0600

    feat: export PartialType from partial-type-helpers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { BaseController } from "./libraries/base-controller";
 export { BaseService } from "./libraries/base-service";
 export { Validator } from "./libraries/validator";
 export { GridFSBucketService } from "./libraries/grid-fs-bucket";
+export { PartialType } from "./libraries/partial-types/partial-type-helpers";
