f7fc7d8dd78e835d7077fc5fe3484a4084ca019f
Elias123Gonzalez <tec4@avalantec.com>
2025-10-20 11:51:10 -0600

    Changed version

--- src/app.ts
+++ src/app.ts
@@ -81,7 +81,7 @@ app.use(catchExceptionMiddleware);
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Welcome to the BIFI App Backend API",
-    version: "202510161358",
+    version: "202510201150",
   });
 });
 
