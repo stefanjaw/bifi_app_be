05c15ef32b1fd0473328862dba7107c604f0223f
Elias123Gonzalez <tec4@avalantec.com>
2025-11-27 11:49:03 -0600

    Removed unnecessary code

--- src/modules/reporting/libraries/reporting-map.ts
+++
@@ -1,6 +0,0 @@
-import { productReportingPaths } from "../../products/libraries/product-reporting-paths.";
-import { IReportingMap } from "../interfaces/reporting";
-
-export const reportingMap: IReportingMap = {
-  Product: productReportingPaths,
-};
