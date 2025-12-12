b3f8ac4c56e410570fb59c0b04b2171018db0d77
Elias123Gonzalez <tec4@avalantec.com>
2025-12-12 14:02:34 -0600

    feat(task-stage): implement create and update methods with transaction handling for default stages, this to only have one default at a time

--- src/modules/task-stages/services/task-stage-service.ts
+++ src/modules/task-stages/services/task-stage-service.ts
@@ -1,11 +1,55 @@
 import { TaskStageDocument } from "@mongodb-types";
-import { BaseService } from "../../../system";
+import { BaseService, runTransaction } from "../../../system";
 import { taskStageModel } from "../models/task-stage.model";
+import { ClientSession } from "mongoose";
+import { TaskStageDTO, UpdateTaskStageDTO } from "../models/task-stage.dto";
 
 export class TaskStageService extends BaseService<TaskStageDocument> {
   constructor() {
     super({
       model: taskStageModel,
     });
+  }
+
+  override async create(
+    data: TaskStageDTO,
+    session?: ClientSession | undefined
+  ): Promise<TaskStageDocument> {
+    return await runTransaction<TaskStageDocument>(
+      session,
+      async (newSession) => {
+        // if isDefault is true, set all other stages to false
+        if (data.isDefault) {
+          await this.model.updateMany(
+            { isDefault: true },
+            { isDefault: false },
+            { session: newSession }
+          );
+        }
+
+        return await super.create(data, newSession);
+      }
+    );
+  }
+
+  override async update(
+    data: UpdateTaskStageDTO,
+    session?: ClientSession | undefined
+  ): Promise<TaskStageDocument> {
+    return await runTransaction<TaskStageDocument>(
+      session,
+      async (newSession) => {
+        // if isDefault is true, set all other stages to false
+        if (data.isDefault) {
+          await this.model.updateMany(
+            { isDefault: true },
+            { isDefault: false },
+            { session: newSession }
+          );
+        }
+
+        return await super.update(data, newSession);
+      }
+    );
   }
 }
