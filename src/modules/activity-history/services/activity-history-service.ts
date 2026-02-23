389407335797673a96920124dc65ae8e6eb92e71
Kim_Uva <tec6@avalantec.com>
2026-02-23 15:00:01 -0600

    feat: Add CSV export functionality to ActivityHistoryService with transaction handling

--- src/modules/activity-history/services/activity-history-service.ts
+++ src/modules/activity-history/services/activity-history-service.ts
@@ -1,7 +1,8 @@
 import { ActivityHistoryDocument } from "@mongodb-types";
 import { activityHistoryModel } from "../models/activity-history.model";
-import { BaseService, UserStore } from "../../../system";
+import { BaseService, runTransaction, UserStore } from "../../../system";
 import { ClientSession } from "mongoose";
+import dayjs, { Dayjs } from "dayjs";
 
 export class ActivityHistoryService extends BaseService<ActivityHistoryDocument> {
   constructor() {
@@ -10,11 +11,37 @@ export class ActivityHistoryService extends BaseService<ActivityHistoryDocument>
 
   override async create(
     data: Record<string, any>,
-    session?: ClientSession | undefined
+    session?: ClientSession | undefined,
   ): Promise<ActivityHistoryDocument> {
     return super.create(
       { ...data, userId: UserStore.getInstance().user?.id },
-      session
+      session,
     );
+  }
+
+  override async exportCSV(data?: Record<string, any>[]): Promise<Buffer> {
+    return runTransaction<Buffer>(undefined, async (newSession) => {
+      const activityHistory = await this.model
+        .find()
+        .populate("userId")
+        .session(newSession);
+
+      const json = activityHistory.map((p) => ({
+        Title: p.title,
+
+        Details: p.details ?? "N/A",
+
+        PerformDate: p.performDate
+          ? dayjs(p.performDate).format("DD MMM YYYY")
+          : "N/A",
+        Model: p.model,
+        User: p.userId ? `${p.userId.username} (${p.userId.email})` : "N/A",
+        CreatedAt: p.createdAt
+          ? dayjs(p.createdAt).format("DD MMM YYYY HH:mm")
+          : "N/A",
+      }));
+
+      return super.exportCSV(json);
+    });
   }
 }
