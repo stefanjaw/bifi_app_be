c9b775810e9dd6f5e17467462ef432589680b392
Elias123Gonzalez <tec4@avalantec.com>
2026-04-09 09:38:05 -0600

    Version Updated

--- src/app.ts
+++ src/app.ts
@@ -219,7 +219,7 @@ app.use("/api", new ReportBugRouter().getRouter);
 app.get("/api/health-check", (req, res) => {
   res.status(200).json({
     message: "Welcome to the BIFI App Backend API",
-    version: "202604081646",
+    version: "202604090937",
     status: "OK",
   });
 });
